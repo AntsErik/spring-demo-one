000714dc9dbfd46313264a96dc8231acff837cce
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-08 13:03:03 +0300

    Step 5> Slide 37

--- src/com/luv2code/springdemo/BasketballCoach.java
+++ src/com/luv2code/springdemo/BasketballCoach.java
@@ -2,10 +2,20 @@ package com.luv2code.springdemo;
 
 public class BasketballCoach implements Coach{
 	
+	private FortuneService fortuneService;
+	
+	public BasketballCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
 		return "Shoot 50 3-pointers!";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune() + " Fly baby, fly!";
 	}
 
 }
--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -2,10 +2,23 @@ package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	public TrackCoach() {
+	}
+
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
 		return "Run a hard 5K!";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just Do It: " + fortuneService.getFortune();
 	}
 
 }
